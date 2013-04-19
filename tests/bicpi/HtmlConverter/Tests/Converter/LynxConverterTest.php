2a6328d83ff4977b08ce61d7f403c3dede493002
Philipp Rieber <p.rieber@gmail.com>
2013-04-19 07:20:57 +0200

    Update copyright information

--- tests/bicpi/HtmlConverter/Tests/Converter/LynxConverterTest.php
+++ tests/bicpi/HtmlConverter/Tests/Converter/LynxConverterTest.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the HtmlConverter library
  *
- * (c) Philipp Rieber <p.rieber@webflips.net>
+ * (c) 2013 Philipp Rieber <p.rieber@webflips.net>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
